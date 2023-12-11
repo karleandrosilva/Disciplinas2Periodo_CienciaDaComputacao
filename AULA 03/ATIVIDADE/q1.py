ab0bc5c10e3b2d39a40519038ad70315d99fd0ec
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2023-12-11 14:38:06 -0300

    New Update Q1

--- AULA 03/ATIVIDADE/q1.py
+++ AULA 03/ATIVIDADE/q1.py
@@ -6,7 +6,7 @@ dia_da_semana = int(input('Informe um número: '))
 if dia_da_semana == 1:
     print('O número que você informou é Domingo.')
 elif dia_da_semana == 2:
-    print('O número que você informou é Segunda-Feir.')
+    print('O número que você informou é Segunda-Feira.')
 elif dia_da_semana == 3:
     print('O número que você informou é Terça-Feira.')
 elif dia_da_semana == 4:
@@ -21,3 +21,20 @@ else:
     print('VALOR INVÁLIDO!')
 
 
+# match dia_da_semana:
+#     case 1:
+#         print('O número que você informou é Domingo.')
+#     case 2:
+#         print('O número que você informou é Segunda-Feira.')
+#     case 3:
+#         print('O número que você informou é Terça-Feira.')
+#     case 4:
+#         print('O número que você informou é Quarta-Feira.')
+#     case 5:
+#         print('O número que você informou é Quinta-Feira.')
+#     case 6:
+#         print('O número que você informou é Sexta-Feira.')
+#     case 7:
+#         print('O número que você informou é Sabádo.')
+#     case _:
+#         print('VALOR INVÁLIDO!')
