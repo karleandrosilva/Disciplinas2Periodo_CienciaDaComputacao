aae42ebb0e915635971bc4aa8d2d2d8f564c329d
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2023-12-19 11:03:36 -0300

    Update q7.py

--- APC-2023.2/AULA 03/estrutura de repetição/estrutura de repetição/lista de exercícios/q7.py
+++ APC-2023.2/AULA 03/estrutura de repetição/estrutura de repetição/lista de exercícios/q7.py
@@ -1,1 +1,13 @@
+'''7. Escreva um programa que receba como entrada dois números e retorne a quantidade
+de números positivos menores que 50 que são múltiplos de ambos.'''
 
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+contador = 0
+
+for numero in range(1, 50):
+    if numero % numero1 == 0 and numero % numero2 == 0:
+        contador += 1
+
+print(f"A quantidade de números positivos menores que 50 e múltiplos de ambos {numero1} e {numero2} é: {contador}")
