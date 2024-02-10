9307e32ca3491a4e31e8a1fc6fa12099f9ed9141
Karleandro <143852351+karleandrosilva@users.noreply.github.com>
2024-02-10 00:14:04 -0300

    update

--- APC-2023.2/AULA 06/Lista/pratica7.py
+++ APC-2023.2/AULA 06/Lista/pratica7.py
@@ -1,7 +1,26 @@
 lista1 = [4, 1, 7, 6, 8, 5]
 lista2 = [1, 2, 3, 4, 5, 6]
-lista3 = []
-cont = 0
+contem = []
+cntAnB = []
+cntBnA = []
+
+for l in lista1:
+    for v in lista2:
+        if l == v:
+            contem.append(l)
+
+for v in lista2:
+    if v not in contem:
+        cntAnB.append(v)
+
+for m in lista1:
+    if m not in contem:
+        cntBnA.append(m)
+
+PEB = (len(contem) / len(lista1)) * 100
+PEA = (len(contem) / len(lista2)) * 100
+
+
 
 if len(lista1) == len(lista2):
     for i in lista1:
